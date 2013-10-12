fa31872275318020e1b600e97f7c03bcffbefa5c
flowmatters <joel@flowmatters.com.au>
2013-10-12 21:39:22 +1100

    Placeholder for a command line runner

---
+++ FlowMatters.Source.VeneerCmd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlowMatters.Source.VeneerCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
