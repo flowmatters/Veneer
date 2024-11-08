1317acd14ce44f680aeb20aa7e5567d665a45865
Joel Rahman <joel.rahman.home@gmail.com>
2024-11-08 11:08:28 +1100

    Option for a veneer configuration file associated with the .rsproj, for listing addons that can be invoked via Reports menu. Currently supporting .exes

---
+++ FlowMatters.Source.Veneer/Addons/VeneerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowMatters.Source.Veneer.Addons
+{
+    public class VeneerConfiguration
+    {
+        public VeneerAddon[] addons;
+    }
+
+    public class VeneerAddon
+    {
+        public string name { get; set; }
+
+        public string type { get; set; }
+
+        public string path { get; set; }
+
+    }
+}
