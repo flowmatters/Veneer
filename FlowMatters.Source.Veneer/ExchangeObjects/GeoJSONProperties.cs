9613adade9d24138d5dd0e264b1151a81b1a94f0
marcustessema <marcus.tessema@ewater.org.au>
2025-05-23 14:53:18 +1000

    RM-20834 RM-21455 GeoJSONProperties reverted back to Serializable so GetObjectData can be used.

--- FlowMatters.Source.Veneer/ExchangeObjects/GeoJSONProperties.cs
+++ FlowMatters.Source.Veneer/ExchangeObjects/GeoJSONProperties.cs
@@ -1,21 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
-namespace FlowMatters.Source.WebServer;
-
-[DataContract]
-public class GeoJSONProperties
+namespace FlowMatters.Source.WebServer
 {
-    public GeoJSONProperties()
+    [Serializable]
+    public class GeoJSONProperties : ISerializable
     {
-            
-    }
+        public GeoJSONProperties()
+        {
 
-    [DataMember]
-    public Dictionary<string, object> properties = new Dictionary<string, object>();
+        }
 
-    public void Add(string key, object value)
-    {
-        properties[key] = value;
+        public GeoJSONProperties(SerializationInfo info, StreamingContext context)
+        {
+        }
+
+        private Dictionary<string, object> properties = new Dictionary<string, object>();
+
+        public void Add(string key, object value)
+        {
+            properties[key] = value;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            foreach (KeyValuePair<string, object> kvp in properties)
+                info.AddValue(kvp.Key, kvp.Value);
+        }
     }
 }
